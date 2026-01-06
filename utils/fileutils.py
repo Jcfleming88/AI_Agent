9e738f7b0cee41db9893e5c9bdc8cff135469f7e
Craig Fleming <jcfleming88@gmail.com>
2026-01-06 14:26:20 +0000

    Image to base64 function added

--- utils/fileutils.py
+++ utils/fileutils.py
@@ -1,4 +1,13 @@
 import pandas as pd
+import base64
 
 def read_json(filepath):
     return pd.read_json(filepath)
+
+def image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        # Read the file and encode it
+        encoded_string = base64.b64encode(image_file.read())
+        
+        # Convert bytes to a UTF-8 string
+        return encoded_string.decode('utf-8')
